0d734622aadfdc43633f4a0e5d4dc8dfc621eb23
myivtah <miftahul354.kjm@gmail.com>
2023-12-01 21:53:12 +0700

    Update Projek

--- app/src/main/java/com/example/travelpariwisata/MainActivity.kt
+++ app/src/main/java/com/example/travelpariwisata/MainActivity.kt
@@ -2,17 +2,37 @@ package com.example.travelpariwisata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val profileFragment = ProfileFragment()
         val homeFragment = HomeFragment()
 
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.fragment_container, homeFragment)
             commit()
         }
+
+        val btnHome = findViewById<ImageButton>(R.id.imageButtonHome)
+        val btnProfile = findViewById<ImageButton>(R.id.imageButtonProfile)
+
+        btnHome.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container, homeFragment)
+                commit()
+            }
+        }
+
+        btnProfile.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container, profileFragment)
+                commit()
+            }
+        }
     }
 }
+
