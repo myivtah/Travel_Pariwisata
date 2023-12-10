a83add89cd7f59c9778f999331605e733b505e95
myivtah <miftahul354.kjm@gmail.com>
2023-12-10 21:42:42 +0700

    Update Projek

--- app/src/main/java/com/example/travelpariwisata/AdminFragment.kt
+++ app/src/main/java/com/example/travelpariwisata/AdminFragment.kt
@@ -1,6 +1,7 @@
+// AdminFragment.kt
+
 package com.example.travelpariwisata
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -16,50 +17,52 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AdminFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AdminFragment : Fragment() {
 
     private lateinit var paketAdminAdapter: PaketAdminAdapter
     private lateinit var paketList: MutableList<PaketModel>
     private val databaseReference = FirebaseDatabase.getInstance().getReference("Paket")
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_admin, container, false)
 
         val rvHome: RecyclerView = view.findViewById(R.id.recyclerPaketAdmin)
         rvHome.layoutManager = GridLayoutManager(requireActivity(), 1)
 
-        // Buat instance dari adapter dan hubungkan dengan RecyclerView
         paketList = mutableListOf()
         paketAdminAdapter = PaketAdminAdapter(requireContext(), paketList)
         rvHome.adapter = paketAdminAdapter
 
-        val btnTambah: Button = view.findViewById(R.id.buttonTambah) // Ganti dengan ID yang sesuai
+        val btnTambah: Button = view.findViewById(R.id.buttonTambah)
 
         btnTambah.setOnClickListener {
-            // Intent untuk berpindah ke activity tambah paket
             val intent = Intent(requireContext(), TambahPaketActivity::class.java)
             startActivity(intent)
         }
 
+        // Menanggapi aksi penghapusan
+        paketAdminAdapter.setOnItemClickListener { paket ->
+            // Ambil URL gambar dari Firebase Realtime Database
+            val imageUrl = paket.imageUrl
+
+            // Hapus data dari Firebase Realtime Database
+            databaseReference.child(paket.id).removeValue()
+
+            // Hapus gambar dari Firebase Storage
+            deleteImageFromStorage(imageUrl, paket.id)
+        }
+
         databaseReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                paketList.clear() // Bersihkan list sebelum menambahkan data baru
+                paketList.clear()
                 for (dataSnapshot in snapshot.children) {
                     val paket = dataSnapshot.getValue(PaketModel::class.java)
                     paket?.let { paketList.add(it) }
@@ -68,7 +71,6 @@ class AdminFragment : Fragment() {
             }
 
             override fun onCancelled(error: DatabaseError) {
-                // Handle error
                 Toast.makeText(
                     requireContext(),
                     "Error: ${error.message}",
@@ -80,16 +82,25 @@ class AdminFragment : Fragment() {
         return view
     }
 
+    private fun deleteImageFromStorage(imageUrl: String, paketId: String) {
+        // Hapus gambar dari Firebase Storage (contoh)
+        val storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(imageUrl)
+        storageReference.delete().addOnSuccessListener {
+            Toast.makeText(
+                requireContext(),
+                "Data dihapus",
+                Toast.LENGTH_SHORT
+            ).show()
+        }.addOnFailureListener {
+            Toast.makeText(
+                requireContext(),
+                "Gagal menghapus gambar: ${it.message}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AdminFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             AdminFragment().apply {
--- app/src/main/java/com/example/travelpariwisata/PaketAdminAdapter.kt
+++ app/src/main/java/com/example/travelpariwisata/PaketAdminAdapter.kt
@@ -8,15 +8,23 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.travelpariwisata.menu.PaketModel
+import com.google.android.material.button.MaterialButton
 import com.squareup.picasso.Picasso
 
 class PaketAdminAdapter(private val context: Context, private val paketList: List<PaketModel>) :
     RecyclerView.Adapter<PaketAdminAdapter.ViewHolder>() {
 
+    private var onItemClickListener: ((PaketModel) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (PaketModel) -> Unit) {
+        onItemClickListener = listener
+    }
+
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val imageView: ImageView = view.findViewById(R.id.imageWisataAdmin)
         val judulTextView: TextView = view.findViewById(R.id.textJudulAdmin)
         val hargaTextView: TextView = view.findViewById(R.id.textViewHargaAdmin)
+        val hapusButton: MaterialButton = view.findViewById(R.id.materialButtonHapus)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -28,10 +36,13 @@ class PaketAdminAdapter(private val context: Context, private val paketList: Lis
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val paket = paketList[position]
 
-        // Set data to views
         Picasso.get().load(paket.imageUrl).into(holder.imageView)
         holder.judulTextView.text = paket.name
         holder.hargaTextView.text = paket.harga.toString()
+
+        holder.hapusButton.setOnClickListener {
+            onItemClickListener?.invoke(paket)
+        }
     }
 
     override fun getItemCount(): Int {
